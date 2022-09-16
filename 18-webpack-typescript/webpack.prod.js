509b63c14e6668be4beaf93de383975310c58b30
Ioannis Merianos <ioannismeri@gmail.com>
2022-09-17 00:37:15 +0300

    webpack prod and dev configs

---
+++ 18-webpack-typescript/webpack.prod.js
@@ -0,0 +1,31 @@
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  mode: "production",
+  entry: "./src/index.ts",
+  devtool: "inline-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "/dist",
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "./"),
+    },
+  },
+  plugins: [new CleanWebpackPlugin()],
+};
